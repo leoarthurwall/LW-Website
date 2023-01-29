e0472ef321aef525fb80b967a0acca9eb8e1c03c
leoarthurwall <leowall93@gmail.com>
2023-01-29 21:00:11 +0000

    added menu nav and footer compponents and imported them to the App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import About from "./Components/About/About";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
+import Menu from "./Components/Menu/Menu";
+import Nav from "./Components/Nav/Nav";
 import Reviews from "./Components/Reviews/Reviews";
 import Services from "./Components/Services/Services";
 import Work from "./Components/Work/Work";
@@ -8,12 +11,15 @@ import Work from "./Components/Work/Work";
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Menu />
       <Home />
       <About />
       <Services />
       <Work />
       <Reviews />
       <Contact />
+      <Footer />
     </div>
   );
 }
