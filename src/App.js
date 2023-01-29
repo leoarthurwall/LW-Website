bab8e67b019edf0abdcb4661c4ff043fa7495bdb
leoarthurwall <leowall93@gmail.com>
2023-01-29 20:57:48 +0000

    added the components to the app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Home from "./Components/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import Services from "./Components/Services/Services";
+import Work from "./Components/Work/Work";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Services />
+      <Work />
+      <Reviews />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
