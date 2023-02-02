993d7f600938cb2f65a1d371a7508bdf72666347
leoarthurwall <leowall93@gmail.com>
2023-02-02 22:11:06 +0100

    Home - set up the layout positioning of the home section

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,13 +3,15 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.textBox}>
-        <h1 className={styles.name}>Hi, I'm Leo</h1>
-        <h1 className={styles.role}>
-          A freelance web developer in Hampshire, England
-          <span className={styles.accent}>.</span>
-        </h1>
+    <section className={styles.home}>
+      <div className={styles.container}>
+        <div className={styles.textBox}>
+          <h1 className={styles.name}>Hi, I'm Leo</h1>
+          <h1 className={styles.role}>
+            A freelance web developer in Hampshire, England
+            <span className={styles.accent}>.</span>
+          </h1>
+        </div>
       </div>
     </section>
   );
