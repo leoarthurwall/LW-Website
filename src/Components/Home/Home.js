2ba1207ca3cd712c388f5b1cbf8e822bbd5ee9e6
leoarthurwall <leowall93@gmail.com>
2023-02-02 22:28:06 +0100

    added vars to all the colors in the home and nav and removed the hi im leo from the home text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,9 +6,8 @@ const Home = () => {
     <section className={styles.home}>
       <div className={styles.container}>
         <div className={styles.textBox}>
-          <h1 className={styles.text}>Hi, I'm Leo..<span className={styles.accent}>.</span></h1>
           <h1 className={styles.text}>
-            A freelance web developer <br /><span className={styles.accent}>&</span> designer in the UK
+            Freelance web developer <br /><span className={styles.accent}>&</span> designer in the UK
             <span className={styles.accent}>.</span>
           </h1>
         </div>
