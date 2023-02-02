443f76a868e4e00c895261cf9314d7dccd2c49cf
leoarthurwall <leowall93@gmail.com>
2023-02-02 22:05:45 +0100

    Home -  set up the basic html

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className={styles.container}>
+      <div className={styles.textBox}>
+        <h1 className={styles.name}>Hi, I'm Leo</h1>
+        <h1 className={styles.role}>
+          A freelance web developer in Hampshire, England
+          <span className={styles.accent}>.</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
