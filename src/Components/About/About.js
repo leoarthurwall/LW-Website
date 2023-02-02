af9b6cd903b3f1ff3281581ebffd085134a6c83e
leoarthurwall <leowall93@gmail.com>
2023-02-02 22:59:06 +0100

    About - createds a left and right container, gave the left position absolutr and a top & left offset of -80 so it overlaps out

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import styles from "./About.module.css"
+import React from "react";
+import styles from "./About.module.css";
 
 const About = () => {
   return (
     <section className={styles.about}>
       <div className={styles.container}>
-
+        <div className={styles.left}></div>
+        <div className={styles.right}></div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
