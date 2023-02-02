05e51c078ec8d41596c61fd9c7943568eadde658
leoarthurwall <leowall93@gmail.com>
2023-02-02 23:10:43 +0100

    About - added basic styling to make layout work with placeholder text. Own text can be added in when ready

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,20 @@ const About = () => {
     <section className={styles.about}>
       <div className={styles.container}>
         <div className={styles.left}></div>
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <h2 className={styles.header}>
+            What I do<span className={styles.accent}>.</span>
+          </h2>
+          <p className={styles.text}>
+            {" "}
+            I am a freelance web designer and front-end developer based in SW4
+            London offering a wide variety of website design and development
+            solutions for SMEs, local businesses and self-employed professionals
+            including informational, promotional and eCommerce websites. Whether
+            you’re looking for a brand new or wish to further develop an
+            existing website, I would be happy to help you achieve your goal.{" "}
+          </p>
+        </div>
       </div>
     </section>
   );
