c9c348d101b8beb542dfffc3935411fd49380423
leoarthurwall <leowall93@gmail.com>
2023-02-03 10:59:39 +0100

    services - mapped through the services array successfully

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Services.module.css";
 
 const Services = () => {
-  const ServicesArray = [
+  const servicesArray = [
     {
       title: "Web Development",
       text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
@@ -28,23 +28,22 @@ const Services = () => {
           My services<span className={styles.accent}>.</span>
         </h2>
         <div className={styles.gridContainer}>
-          <div className={styles.gridItem}>
-            <div className={styles.upper}>
-              <div className={styles.circle}></div>
-              <h3 className={styles.subHeader}>
-                Web Development<span className={styles.accent}>.</span>{" "}
-              </h3>
-            </div>
-            <div className={styles.lower}>
-              <p className={styles.text}>
-                My main area of expertise lies within front-end development but
-                can also offer back-end development to deliver any web
-                functionalities to suit all your project needs. This could be by
-                building a brand new website or perhaps by adding extra features
-                to an exiting one.
-              </p>
-            </div>
-          </div>
+          {servicesArray.map((service, id) => {
+            return (
+              <div className={styles.gridItem}>
+                <div className={styles.upper}>
+                  <div className={styles.circle}></div>
+                  <h3 className={styles.subHeader}>
+                    {service.title}
+                    <span className={styles.accent}>.</span>{" "}
+                  </h3>
+                </div>
+                <div className={styles.lower}>
+                  <p className={styles.text}>{service.text}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
