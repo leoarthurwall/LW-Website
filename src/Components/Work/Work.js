a9a763e7bcf71e15cff1978f9ebb51f27855e527
leoarthurwall <leowall93@gmail.com>
2023-02-07 10:04:59 +0100

    created an images folder and added 3 stock inages for the projects

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,30 +1,20 @@
 import React from "react";
 import styles from "./Work.module.css";
-import { BiCodeAlt } from "react-icons/bi";
-import { MdDesignServices } from "react-icons/md";
-import { AiTwotoneSetting } from "react-icons/ai";
-import { GiTestTubes } from "react-icons/gi";
+
 
 const Work = () => {
   const workArray = [
     {
-      title: "Web Development",
-      icon: BiCodeAlt,
+      title: "JM Exteriors",
+      text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
+      bgImage: "",
+    },
+    {
+      title: "Inov-8",
       text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
     },
     {
-      title: "Web Design",
-      icon: MdDesignServices,
-      text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
-    },
-    {
-      title: "Web Maintenance",
-      icon: AiTwotoneSetting,
-      text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
-    },
-    {
-      title: "Web Optimisation",
-      icon: GiTestTubes,
+      title: "Pearsons",
       text: "My main area of expertise lies within frontend development but can also offer backend development to deliver any web functionalities to suit all your project needs. This could be by building a brand new website or perhaps by adding extra features to an exiting one.",
     },
   ];
@@ -36,23 +26,20 @@ const Work = () => {
           My Work<span className={styles.accent}>.</span>
         </h2>
         <div className={styles.gridContainer}>
-          {workArray.map((work, id) => {
-            const Icon = work.icon;
+          {workArray.map((work, index) => {
             return (
               <div className={styles.gridItem}>
-                <div className={styles.upper}>
-                  <div className={styles.circle}>
-                    <Icon size={32}/>
-                  </div>
-                  <h3 className={styles.subHeader}>
-                    {work.title}
-                    <span className={styles.accent}>.</span>{" "}
-                  </h3>
-                </div>
-                <div className={styles.lower}>
-                  <p className={styles.text}>{work.text}</p>
-                </div>
+              <img
+                key={index}
+                src={work.bgImage}
+                alt={work.title}
+                className={styles.image}
+              ></img>
+              <div className={styles.imageTextWrapper}>
+                <h2 className={styles.itemHeader}>{work.title}</h2>
+                <p className={styles.itemText}>{work.text}</p>
               </div>
+            </div>
             );
           })}
         </div>
