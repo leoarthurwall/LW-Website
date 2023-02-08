d77ef40dcb4dfc1cfc868596ea9b2a1581eee449
leoarthurwall <leowall93@gmail.com>
2023-02-08 10:37:57 +0100

    footer - laid out left section of footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,20 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <section className={styles.footer}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.textWrapper}>
+            <h3 className={styles.header}>
+              About<span className={styles.accent}>.</span>
+            </h3>
+            <p className={styles.text}>
+              Making great ideas a reality on the web.
+            </p>
+          </div>
+          <p className={styles.text}>© 2022 LW Web Studio.</p>
+        </div>
+        <div className={styles.right}></div>
+      </div>
     </section>
   );
 };
