94009212f7059ded5c00f3ec12c91985ed4c8bd5
leoarthurwall <leowall93@gmail.com>
2023-02-08 10:05:21 +0100

    contact - added placeholder text to work on layout

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className={styles.contact}>
+      <div className={styles.container}>
+        <h2 className={styles.header}>
+          Contact<span className={styles.accent}>.</span>
+        </h2>
+        <p className={styles.text}>
+          As a freelance web developer, I'm committed to providing you with a
+          high-quality service tailored to your personal needs. I am currently
+          available to take on new work, so if you're interested in working
+          together, please feel free to get in contact with by email or phone.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
