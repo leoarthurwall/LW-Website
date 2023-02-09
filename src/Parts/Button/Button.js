8139551b9ed45b1ed2df0eddce4ce5802470490e
leoarthurwall <leowall93@gmail.com>
2023-02-09 17:39:09 +0100

    created a button component that I will style and then enable dynamic styling

---
+++ src/Parts/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <p className={styles.text}>Button</p>
+      </div>
+    </>
+  );
+};
+
+export default Button;
