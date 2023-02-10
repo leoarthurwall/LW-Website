a5a2bbf4ef7528d0ae0316cb323523de3b3ea021
leoarthurwall <leowall93@gmail.com>
2023-02-10 10:06:34 +0100

    nav - conditionally display menu icon at breakpoint of 992px

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Logo from "../../Parts/Logo/Logo";
 import styles from "./Nav.module.css";
+import { HiMenu } from "react-icons/hi";
+
+import { useMediaQuery } from "react-responsive";
 
 const Nav = () => {
+  const isTablet = useMediaQuery({ query: "(max-width: 992px)" });
+
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
-      <Logo fontSize={"36px"} color={"var(--black-color)"}/>
-        <ul className={styles.list}>
-          <li className={styles.item}>About Me</li>
-          <li className={styles.item}>My Services</li>
-          <li className={styles.item}>Previous Work</li>
-          <li className={styles.item}>Get in Touch</li>
-        </ul>
+        <Logo fontSize={"36px"} color={"var(--black-color)"} />
+        {isTablet ? (
+          <span className={styles.iconWrapper}>
+            <HiMenu />
+          </span>
+        ) : (
+          <ul className={styles.list}>
+            <li className={styles.item}>About Me</li>
+            <li className={styles.item}>My Services</li>
+            <li className={styles.item}>Previous Work</li>
+            <li className={styles.item}>Get in Touch</li>
+          </ul>
+        )}
       </div>
     </nav>
   );
