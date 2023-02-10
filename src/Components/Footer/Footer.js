5e13c4aa5d258d639764f8018e08956cd4ae615c
leoarthurwall <leowall93@gmail.com>
2023-02-10 11:55:52 +0100

    footer - put left and right in an upper container and then the address and copyright in a lower container, this will assist with responsiveneess

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,56 +6,60 @@ const Footer = () => {
   return (
     <section className={styles.footer}>
       <div className={styles.container}>
-        <div className={styles.left}>
-          <div className={styles.textWrapper}>
-            <Logo fontSize={"28px"} color={"var(--secondary-color)"} />
-            <p className={styles.text}>
-              Here to bring your great ideas to life through quality web design and
-              development.
-            </p>
+        <div className={styles.upper}>
+          <div className={styles.left}>
+            <div className={styles.textWrapper}>
+              <Logo fontSize={"28px"} color={"var(--secondary-color)"} />
+              <p className={styles.text}>
+                Here to bring your great ideas to life through quality web
+                design and development.
+              </p>
+            </div>
           </div>
-          <p className={styles.lowerText}>© 2023 LW Web Studio.</p>
+          <div className={styles.right}>
+            <div className={styles.rightUpper}>
+              <div className={styles.rightColumn}>
+                <div className={styles.textWrapper}>
+                  <h3 className={styles.header}>
+                    Sitemap<span className={styles.accent}>.</span>
+                  </h3>
+                  <ul className={styles.list}>
+                    <li className={styles.item}>About Me</li>
+                    <li className={styles.item}>My services</li>
+                    <li className={styles.item}>Previous Work</li>
+                    <li className={styles.item}>Contact</li>
+                  </ul>
+                </div>
+              </div>
+              <div className={styles.rightColumn}>
+                <div className={styles.textWrapper}>
+                  <h3 className={styles.header}>
+                    Say hello<span className={styles.accent}>.</span>
+                  </h3>
+                  <ul className={styles.list}>
+                    <li className={styles.item}>Phone</li>
+                    <li className={styles.item}>Email</li>
+                    <li className={styles.item}>Contact Form</li>
+                    <li className={styles.item}>Whatsapp</li>
+                  </ul>
+                </div>
+              </div>
+              <div className={styles.rightColumn}>
+                <div className={styles.textWrapper}>
+                  <h3 className={styles.header}>
+                    Useful<span className={styles.accent}>.</span>
+                  </h3>
+                  <ul className={styles.list}>
+                    <li className={styles.item}>FAQs</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.right}>
-          <div className={styles.rightUpper}>
-            <div className={styles.rightColumn}>
-              <div className={styles.textWrapper}>
-                <h3 className={styles.header}>
-                  Sitemap<span className={styles.accent}>.</span>
-                </h3>
-                <ul className={styles.list}>
-                  <li className={styles.item}>About Me</li>
-                  <li className={styles.item}>My services</li>
-                  <li className={styles.item}>Previous Work</li>
-                  <li className={styles.item}>Contact</li>
-                </ul>
-              </div>
-            </div>
-            <div className={styles.rightColumn}>
-              <div className={styles.textWrapper}>
-                <h3 className={styles.header}>
-                  Say hello<span className={styles.accent}>.</span>
-                </h3>
-                <ul className={styles.list}>
-                  <li className={styles.item}>Phone</li>
-                  <li className={styles.item}>Email</li>
-                  <li className={styles.item}>Contact Form</li>
-                  <li className={styles.item}>Whatsapp</li>
-                </ul>
-              </div>
-            </div>
-            <div className={styles.rightColumn}>
-              <div className={styles.textWrapper}>
-                <h3 className={styles.header}>
-                  Useful<span className={styles.accent}>.</span>
-                </h3>
-                <ul className={styles.list}>
-                  <li className={styles.item}>FAQs</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+        <div className={styles.lower}>
           <p className={styles.lowerText}>Southsea, Hampshire, UK, PO4. </p>
+          <p className={styles.lowerText}>© 2023 LW Web Studio.</p>
         </div>
       </div>
     </section>
