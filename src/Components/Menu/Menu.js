925e943c4ed83062556aba2cd63d3ca3b0f9e48c
leoarthurwall <leowall93@gmail.com>
2023-02-13 19:21:34 +0000

    menu - added the jsx

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import styles from "./Menu.module.css";
 
-const Menu = () => {
+const Menu = ({ isOpen, handleMenuClick }) => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className={isOpen ? styles.containerOpen : styles.containerClosed}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <a href="#home" onClick={handleMenuClick}>
+            Home
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href="#about" onClick={handleMenuClick}>
+            About
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href="#services" onClick={handleMenuClick}>
+            Services
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href="#contact" onClick={handleMenuClick}>
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
